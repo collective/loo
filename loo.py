4f8c4eeb476d77ce3242d497b94a635ecafe8668
Alex Clark <aclark@aclark.net>
2010-12-13 23:45:31 -0500

    Add toilet icon (sort of, it's actually a guy on a bench reading RSS)

--- loo.py
+++ loo.py
@@ -19,27 +19,25 @@ class LoginOnlyOncePlugin(BasePlugin):
         self.title = title
 
 
-def manage_addpackage(dispatcher, id, title=None, REQUEST=None):
+def add_plugin(dispatcher, id, title=None, REQUEST=None):
     """
     Boo yah! You need this doc string or Zope 2's publisher will drop this
     method faster than you can say: "Page not found". During this time you
-    will also be wondering what is going on until you finally get around to
-    deleting the ignored exceptions in /Plone/error_log.
+    will also be wondering what is going on, until you finally get around to
+    deleting the ignored exceptions in /Plone/error_log and discovering
+    that you need this doc string.
     """
     plugin = LoginOnlyOncePlugin(id, title)
     dispatcher._setObject(plugin.getId(), plugin)
     if REQUEST is not None:
-        REQUEST['RESPONSE'].redirect('%s/manage_workspace'
-                                     '?manage_tabs_message='
-                                     'packageHelper+added.'
-                                      % dispatcher.absolute_url())
+        REQUEST['RESPONSE'].redirect('%s/manage_workspace?manage_tabs_message='
+            'LoginOnlyOncePlugin+added.' % dispatcher.absolute_url())
 
 
 registerMultiPlugin(LoginOnlyOncePlugin.meta_type)
 
 
 def initialize(context):
-    manage_addform = PageTemplateFile('add_plugin', globals(),
-        __name__='manage_addform')
+    pt = PageTemplateFile('add_plugin', globals(), __name__='manage_addform')
     context.registerClass(LoginOnlyOncePlugin, permission=manage_users,
-        constructors=(manage_addform, manage_addpackage), visibility=None)
+        constructors=(pt, add_plugin), visibility=None, icon='loo.png')
