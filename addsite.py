4b805125462aaa3c25fd67eacecfdcdc3be598c9
Alex Clark <aclark@aclark.net>
2010-12-15 13:09:32 -0500

    Add a site

--- addsite.py
+++ addsite.py
@@ -1,8 +1,19 @@
 
 from sys import argv
+from Products.CMFPlone.factory import addPloneSite
+
+def _setup_app(app, admin):
+    from AccessControl.SecurityManagement import newSecurityManager
+    from Testing.makerequest import makerequest
+    admin = app.acl_users.getUser(admin)
+    admin = admin.__of__(app.acl_users)
+    newSecurityManager(None, admin)
+    app = makerequest(app)
+    return app
 
 def addsite(app, admin, site):
-    print app, admin, site
+    app = _setup_app(app, admin)
+    addPloneSite(app, site)
 
 def adduser(site, user, pw):
     print site, user, pw
