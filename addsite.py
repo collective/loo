3fdc76aeeb6e7e049b2ba8342d3bf773c6fdf39d
Alex Clark <aclark@aclark.net>
2010-12-15 13:36:18 -0500

    Add usage

--- addsite.py
+++ addsite.py
@@ -21,7 +21,7 @@ def adduser(app, site, user, pw):
 
 if __name__ == '__main__':
     if len(argv) != 5:
-        print "Usage: "
+        print "Usage: bin/plone run addsite.py <zope_admin> <plone_site> <user> <password>"
     else:
         admin, site, user, pw = argv[1:5]
         addsite(app, admin, site)
